2f695484ed2438589c5da24a5f49ec11dafc2c80
Levi Goddard <levigoddard@users.noreply.github.com>
2017-09-28 23:51:37 -0600

    Week 3 work

---
+++ hw-3/sketch.js
@@ -0,0 +1,17 @@
+// Declare the setup function
+function setup() {
+
+    // create a canvas 800px wide and 800px high
+    createCanvas( 800, 800 );
+
+    // color the background 'grey'
+    background( 'grey' );
+}
+
+// Declare the draw function
+function draw() {
+
+    // tell p5 to print the text string "Hello World!"
+    // This is going to be positioned at ( x: 60, y: 40 ).
+    text( "Hello World!", 30, 40 );
+}
